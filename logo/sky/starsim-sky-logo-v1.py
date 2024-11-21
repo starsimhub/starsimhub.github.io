624add101498470d3fabc4a874784817f8405871
Cliff Kerr <questionably@gmail.com>
2024-11-21 14:13:20 -0500

    working

--- logo/sky/starsim-sky-logo-v1.py
+++ logo/sky/starsim-sky-logo-v1.py
@@ -13,38 +13,51 @@ class Dots(sc.prettyobj):
 
     def __init__(self):
         self.seed = 1
-        self.npts = 10
-        self.niter = 200
-        self.v_center = 1e-2
-        self.v_neigh = 1e-1
+        self.npts = 60
+        self.niter = 1000
+        self.mindist = 0.2
+        self.v_center = 3e-3
+        self.v_neighbor = 3e-2
+        self.temp = 0.05
 
         np.random.seed(self.seed)
         self.x = np.random.uniform(-1, 1, size=self.npts)
         self.y = np.random.uniform(-1, 1, self.npts)
         self.step = 0
+        self.trim()
         return
 
     def xy(self):
         return self.x, self.y
 
+    def trim(self):
+        x,y = self.xy()
+        r = x**2+y**2
+        valid = r<=1.0
+        self.x = self.x[valid]
+        self.y = self.y[valid]
+        self.npts = valid.sum()
+        return
+
     def all_dists(self, i):
         x,y = self.xy()
         return ((x-x[i])**2 + (y-y[i])**2)**0.5
 
-    def min_dist(self, i):
+    def nearest(self, i):
         x,y = self.xy()
         dists = self.all_dists(i)
+        dists[i] = np.inf
+        assert dists.min() > 0
         ind = np.argmin(dists)
         return sc.dictobj(ind=ind, dist=dists[ind], x=x[ind], y=y[ind])
 
-    def diff2vec(self, i, x1, y1, x2, y2, sign=1):
+    def diff2vec(self, i, x1, y1, x2, y2):
         xdiff = x2 - x1
         ydiff = y2 - y1
-        mag = 1.0#(xdiff**2 + ydiff**2)**0.5
-        xcomp = xdiff/mag*sign
-        ycomp = ydiff/mag*sign
-        # print(f'{self.step=}, {i=}, {x2=}, {y2=}, {xdiff=}, {ydiff=}, {mag=}, {xcomp=}, {ycomp=}')
-        return xcomp, ycomp
+        return -xdiff, -ydiff
+
+    def noise(self, arg):
+        return arg*sc.perturb(len(arg), span=self.temp)
 
     def to_center(self):
         x,y = self.xy()
@@ -52,17 +65,32 @@ class Dots(sc.prettyobj):
         dx = np.zeros(npts)
         dy = np.zeros(npts)
         for i in range(npts):
-            dx[i], dy[i] = self.diff2vec(i, 0, 0, x[i], y[i], sign=-1)
-        print(dx, dy)
-        self.x += dx*self.v_center
-        self.y += dy*self.v_center
+            dx[i], dy[i] = self.diff2vec(i, 0, 0, x[i], y[i])
+        self.x += self.noise(dx*self.v_center)
+        self.y += self.noise(dy*self.v_center)
         return
 
-    def run(self, animate=0.1):
+    def push_neighbor(self):
+        x,y = self.xy()
+        npts = self.npts
+        dx = np.zeros(npts)
+        dy = np.zeros(npts)
+        for i in range(npts):
+            near = self.nearest(i)
+            # print(near.dist)
+            if near.dist < self.mindist:
+                # print('YES', i)
+                dx[i], dy[i] = self.diff2vec(i, near.x, near.y, x[i], y[i])
+        self.x += self.noise(-dx*self.v_neighbor)
+        self.y += self.noise(-dy*self.v_neighbor)
+        return
+
+    def run(self, animate=0):
         fig = None
         for i in range(self.niter):
             self.step += 1
             self.to_center()
+            self.push_neighbor()
             if animate:
                 fig = self.plot(fig, animate)
         return
@@ -72,6 +100,7 @@ class Dots(sc.prettyobj):
             fig = plt.figure(dpi=300)
         else:
             plt.clf()
+        # plt.scatter([0], [0], c='k', s=300)
         plt.scatter(self.x, self.y, s=150)
         plt.axis('square')
         plt.xlim(left=-1, right=1)
@@ -87,5 +116,6 @@ class Dots(sc.prettyobj):
 
 dots = Dots()
 dots.run()
+dots.plot()
 
 
